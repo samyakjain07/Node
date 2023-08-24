309cacab5ed649e6259d3a6b77d42bfd8c560e0c
Samyak Jain <samyak811@gmail.com>
2023-08-24 23:01:35 +0530

    changes

---
+++ add.js
@@ -0,0 +1,7 @@
+const add = (a,b) =>{
+    return a+b;
+};
+const sum = add(2,2);
+console.log(sum);
+
+module.exports = add;
---
+++ index.js
@@ -0,0 +1,5 @@
+const add= require("./add.js");
+console.log("Hello From idex.js");
+
+const sum= add(1,2);
+console.log(sum);
